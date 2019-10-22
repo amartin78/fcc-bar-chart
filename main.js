8716defea834ddc91ee1a398e85a5fbbaa6659d6
antonio <ammj24@hotmail.com>
2019-10-22 19:06:59 +0100

    dataset and position

--- main.js
+++ main.js
@@ -1,18 +1,18 @@
 
 
 d3.json('https://raw.githubusercontent.com/freeCodeCamp/ProjectReferenceData/master/GDP-data.json')
-					.then(data => {
-						dataset = data['data'];
-						chart(dataset);
-					});
-
-
-
+	.then(result => {
+		dataset = result['data'];
+		chart(dataset);
+	});
 
 
 function chart(dataset) {
 
-	console.log(dataset);
+	// dataset = dataset.filter(d => {
+	// 	return (parseInt(d[0].substr(0,4)) % 5 === 0 && d[0][6] === '7');
+	// });
+	// console.log(dataset);
 
 	d3.select('body')
 	.append('h1')
@@ -21,15 +21,16 @@ function chart(dataset) {
 	.text('United States GDP');
 
 
-	const width = 500;
+	const width = 1100;
 	const height = 500;
+	const padding = 60;
 
 	const xScale = d3.scaleLinear()
-						.domain([0, 200])
-						.range([0, 200]);
+						.domain([0, d3.max(dataset, (d) => parseInt(d[0]))])
+						.range([padding, width - padding]);
 	const yScale = d3.scaleLinear()
-						.domain([0, 200])
-						.range([0, 200]);
+						.domain([0, d3.max(dataset, (d) => d[1])])
+						.range([height - padding, padding]);
 
 	const xAxis = d3.axisBottom(xScale);
 	const yAxis = d3.axisLeft(yScale);
@@ -37,16 +38,16 @@ function chart(dataset) {
 	const svg = d3.select('body')
 					.append('svg')
 					.attr('width', width)
-					.attr('height', height);
+					.attr('height', height)
 				
 	svg.append('g')
 		.attr('id', 'x-axis')
-		.attr('transform', 'translate(0, 1)')
+		.attr('transform', 'translate(0,' + (height - padding) + ')')
 		.call(xAxis);
 
 	svg.append('g')
 		.attr('id', 'y-axis')
-		.attr('transform', 'translate(0, -10)')
+		.attr('transform', 'translate(60, 0)')
 		.call(yAxis);
 
 	svg.selectAll('rect')
@@ -54,6 +55,15 @@ function chart(dataset) {
 		.enter()
 		.append('rect')
 		.attr('class', 'bar')
+		.attr('fill', 'teal')
+		.attr('data-date', (d) => d[0])
+		.attr('data-gdp', (d) => d[1])
+		.attr('x', (d, i) => i * 3.8 + padding)
+		.attr('y', (d) => {
+			return height - padding - (d[1]/50);
+		})
+		.attr('width', 2.8)
+		.attr('height', (d) => d[1]/50);
 
 }
 	
